03a16edac70cc0c3bf294cba2af4103602943c62
Eduhh <frazaoe620@gmail.com>
2026-08-22 16:02:20 -0300

    Refactor User model by updating fields
    Removed username and usernameLower fields, added handle and nick fields with descriptions.

--- app/src/main/java/com/discord2/app/models/User.kt
+++ app/src/main/java/com/discord2/app/models/User.kt
@@ -2,7 +2,7 @@ package com.discord2.app.models
 
 data class User(
     val uid: String = "",
-    val username: String = "",
-    val usernameLower: String = "",
+    val handle: String = "",   // ex: "user2282" - fixo, gerado automaticamente, usado pra adicionar
+    val nick: String = "",     // ex: "Edudh" - nome de exibição, editável a qualquer momento
     val email: String = ""
 )
